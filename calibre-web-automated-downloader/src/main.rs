f5a7f875e057dcfdd5d2a6c1193d5f0312d37e9d
Mustafa Soylu <m.soylu@fz-juelich.de>
2024-12-24 00:37:39 +0100

    fix main to use and add network crate

--- calibre-web-automated-downloader/src/main.rs
+++ calibre-web-automated-downloader/src/main.rs
@@ -1,12 +1,11 @@
 mod config;
+mod network;
 
 use config::CONFIG;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Access configuration settings using the global CONFIG instance
-    let config = CONFIG.as_ref().expect("Failed to load configuration");
-
-    println!("Base Directory: {:?}", config.base_dir);
+    println!("STATUS_TIMEOUT: {:?}", CONFIG.status_timeout);
 
     Ok(())
 }
